0a692040d985cf8b383cff0410ac52d02cbed2d2
Subhash Kumar <142812196+FZRAJPUT@users.noreply.github.com>
2024-10-01 19:37:38 +0530

    Update executePy.js

--- server/executePy.js
+++ server/executePy.js
@@ -4,7 +4,7 @@ const { exec } = require('child_process');
 const executePy = (filepath) => {
 
     return new Promise((resolve, reject) => {
-        exec(`python ${filepath}`, (error, stdout, stderr) => {
+        exec(`python ${filepath.replace(/\\/g, '/')}`, (error, stdout, stderr) => {
             error && reject({error, stderr});
             stderr && reject({stderr});
             resolve(stdout);
